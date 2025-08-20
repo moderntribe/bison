8a42cbaab04d1ea09178e8fecab790b3fa536ae0
Justin Rhodes <justinr@tri.be>
2025-08-20 10:27:41 -0500

    Add Mailgun support and implement test email functionality

---
+++ app/Console/Commands/TestMailDriver.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\TestEmail;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class TestMailDriver extends Command implements PromptsForMissingInput
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-mail-driver {recipientEmail : The email address to send the test mail to}';
+
+    protected $description = 'Send a test mail to the specified email address';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        if (App::environment('production')) {
+            $this->error('This command is not allowed in production');
+
+            return;
+        }
+
+        // validate the email address
+        $validator = Validator::make($this->arguments(), [
+            'recipientEmail' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            $this->error($validator->errors()->first());
+
+            return;
+        }
+
+        Mail::to($validator->safe()->input('recipientEmail'))->send(new TestEmail);
+
+        $this->info('Test email sent');
+    }
+
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'recipientEmail' => 'Enter the email address to send the test mail to',
+        ];
+    }
+}
---
+++ app/Mail/TestEmail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class TestEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Test Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.test',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
