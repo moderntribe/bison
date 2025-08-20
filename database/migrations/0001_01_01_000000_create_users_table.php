fb8ab31af2dd507816e127168b1fde79ccfd04b7
Justin Rhodes <justinr@tri.be>
2025-08-20 11:38:22 -0500

    Add app_authentication_secret field to users table and implement CreateUser command for user creation

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->string('invite_token')->nullable();
+            $table->text('app_authentication_secret')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
