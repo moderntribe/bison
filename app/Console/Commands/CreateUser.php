6a0fe513313a3b27cab368418806ccc0a92446ec
Justin Rhodes <justinr@tri.be>
2025-09-22 12:44:35 -0500

    Add missing import for Hash facade in CreateUser command

--- app/Console/Commands/CreateUser.php
+++ app/Console/Commands/CreateUser.php
@@ -7,6 +7,7 @@ use App\Models\Role;
 use App\Models\User;
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
 
 use function Laravel\Prompts\password;
 use function Laravel\Prompts\select;
